e25f8f9bbc31dddd79547d7e31a0c826732c33a9
Jelani Sanders <jelanisands@gmail.com>
2023-01-11 00:27:04 -0500

    Added a Sssssnaaaaaakegit add client.js

--- client.js
+++ client.js
@@ -5,12 +5,17 @@ const connect = function() {
     host: "localhost",
     port: 50541,
   });
+  //Takes in data from the server
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
+  //gives alert to the user once connection is established
   conn.on("connect", () =>
     console.log("Connected to game server!")
   );
+  conn.on("connect", () => {
+    conn.write("Name: SNK");
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   return conn;
