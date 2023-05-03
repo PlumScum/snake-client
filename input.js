d3661f1aa79d09a0656a4c650ff6df43d6d8da89
Jelani Sanders <jelanisands@gmail.com>
2023-05-02 22:51:40 -0400

    Move input mapping to constants.js. Import mappings from constants.js

--- input.js
+++ input.js
@@ -1,20 +1,5 @@
-
-// Map key strokes to the write command string
-const mappings = {
-  w: "Move: up",
-  a: "Move: left",
-  s: "Move: down",
-  d: "Move: right",
-  t: "Say: It's futile!",
-  r: "Say: Insatiable...",
-  y: "Say: Why are you running?",
-  e: "Say: Fe fi fo...",
-  q: "Say: Give up!",
-  u: "Say: I got all day",
-  i: "Say: Run!",
-  o: "Say: Hello there",
-  p: "Say: Time to eat"
-};
+// Import our mappings for input
+const { mappings } = require("./constants");
 
 const handleUserInput = (key) => {
   // Short circuit if user does the Ctrl-C key
@@ -23,7 +8,7 @@ const handleUserInput = (key) => {
   }
   
   
-    // handles all other key strokes
+  // handles all other key strokes
   if (mappings[key]) {
     connection.write(mappings[key]);
   }
@@ -38,9 +23,12 @@ const setupInput = function(conn) {
   stdin.resume();
   stdin.on("data", (data) => {
     handleUserInput(data);
+    handleUserInput(data);
   });
   return stdin;
 };
 
 
+
 module.exports = setupInput;
+
